868269c20ac23afe9a2772db9bf85d9a44fdaf37
Avaz <86311939+sb-dor@users.noreply.github.com>
2026-09-21 21:53:38 +0500

    minor changes

--- test/variant_1_test.dart
+++ test/variant_1_test.dart
@@ -9,7 +9,7 @@ void main() {
       // This code defines a createContainer function which creates that storage. Ideally, you should create this for each test
       // Setting up the container with the right overrides for each test can be error-prone.
       // Misconfiguration can lead to tests that pass incorrectly (false positives) or fail for reasons unrelated to the test's intent.
-      // But what if there are a lot of providers? And they all depend on one another. At the same time, a single supplier encompasses ten others... and each of those ten has ten more.
+      // But what if there are a lot of providers? And they all depend on one another. At the same time, a single provider encompasses ten others... and each of those ten has ten more.
       // So what you should do at that moment? You will add those providers inside this overrides?
       final container = createContainer(
         overrides: [
